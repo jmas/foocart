78a00a34facb2b0e3042300e0ced124ae6ed58d6
Alex Maslakov <alex@Users-MacBook-Pro.local>
2020-10-15 18:33:05 +0300

    Add styles

--- src/render.ts
+++ src/render.ts
@@ -1,5 +1,6 @@
 import { html, render as litRender } from 'lit-html';
 import { ICart, IOrder, IOutput, ITranslation, OrderItemIDType, SectionType, IFieldProps, ITextFieldProps, ITextareaFieldProps } from './types';
+import styles from './styles.module.css';
 
 export default function render(cart: ICart, output: IOutput, translation: ITranslation, element: HTMLElement, priceTemplate: string) {
     function handleAddItemClick(id: OrderItemIDType) {
@@ -47,68 +48,76 @@ export default function render(cart: ICart, output: IOutput, translation: ITrans
     }
 
     litRender(html`
-        ${cart.section === SectionType.BUTTON ? html`
-            <button type="button" @click=${handleCartButtonClick}>${output.items.length}</button>
-        ` : cart.section === SectionType.CART ? html`
-            <div>
-                ${output.items.length > 0 ? output.items.map(item => html`
-                    <div key=${item.id}>
-                        <div><b>${item.name}</b></div>
-                        <div>
-                            <span>${translation.count}: <b>${item.count}</b></span>
-                            <span>${translation.price}: <b>${formatPrice(item.price)}</b></span>
-                        </div>
-                        <div>
-                            <button type="button" @click=${() => handleAddItemClick(item.id)}>${translation.addItem}</button>
-                            <button type="button" @click=${() => handleRemoveItemClick(item.id)}>${translation.removeItem}</button>
-                        </div>
-                    </div>
-                `) : (html`
-                    <div>${translation.nothingHere}</div>
-                `)}
-            </div>
-            <button type="button" @click=${handleMakeOrderClick}>${translation.makeOrder} (${formatPrice(output.totalPrice)})</button>
-            <button type="button" @click=${handleCloseClick}>${translation.close}</button>
-        ` : cart.section === SectionType.ORDER ? html`
-            <form @submit=${handleOrderSubmit}>
-                ${output.orderResult.message ? html`<div>${output.orderResult.message}</div>` : null}
-                ${textField({
-                    label: translation.name,
-                    name: 'name',
-                    error: output.orderResult.errors?.name
-                })}
-                ${textField({
-                    label: translation.surname,
-                    name: 'surname',
-                    error: output.orderResult.errors?.surname
-                })}
-                ${textField({
-                    label: translation.telephone,
-                    name: 'telephone',
-                    type: 'tel',
-                    error: output.orderResult.errors?.telephone
-                })}
-                ${textField({
-                    label: translation.email,
-                    name: 'email',
-                    error: output.orderResult.errors?.email
-                })}
-                ${textareaField({
-                    label: translation.comment,
-                    name: 'comment',
-                    error: output.orderResult.errors?.comment
-                })}
-                <button type="submit" ?disabled=${output.orderResult.pending}>
-                    ${translation.approveOrder} (${formatPrice(output.totalPrice)})
+        <div class=${styles.cart}>
+            ${cart.section === SectionType.BUTTON ? html`
+                <button
+                    type="button"
+                    @click=${handleCartButtonClick}
+                    class=${styles.cartButton}
+                >
+                    <span>${output.items.length}</span>
                 </button>
-            </form>
-            <button type="button" @click=${handleCloseClick}>${translation.close}</button>
-        ` : cart.section === SectionType.COMPLETE ? html`
-            <div>
-                ${translation.orderCompleted}
-            </div>
-            <button type="button" @click=${handleCloseClick}>${translation.close}</button>
-        ` : null}
+            ` : cart.section === SectionType.CART ? html`
+                <div>
+                    ${output.items.length > 0 ? output.items.map(item => html`
+                        <div key=${item.id}>
+                            <div><b>${item.name}</b></div>
+                            <div>
+                                <span>${translation.count}: <b>${item.count}</b></span>
+                                <span>${translation.price}: <b>${formatPrice(item.price)}</b></span>
+                            </div>
+                            <div>
+                                <button type="button" @click=${() => handleAddItemClick(item.id)}>${translation.addItem}</button>
+                                <button type="button" @click=${() => handleRemoveItemClick(item.id)}>${translation.removeItem}</button>
+                            </div>
+                        </div>
+                    `) : (html`
+                        <div>${translation.nothingHere}</div>
+                    `)}
+                </div>
+                <button type="button" @click=${handleMakeOrderClick} ?disabled=${output.items.length === 0}>${translation.makeOrder} (${formatPrice(output.totalPrice)})</button>
+                <button type="button" @click=${handleCloseClick}>${translation.close}</button>
+            ` : cart.section === SectionType.ORDER ? html`
+                <form @submit=${handleOrderSubmit}>
+                    ${output.orderResult.message ? html`<div>${output.orderResult.message}</div>` : null}
+                    ${textField({
+                        label: translation.name,
+                        name: 'name',
+                        error: output.orderResult.errors?.name
+                    })}
+                    ${textField({
+                        label: translation.surname,
+                        name: 'surname',
+                        error: output.orderResult.errors?.surname
+                    })}
+                    ${textField({
+                        label: translation.telephone,
+                        name: 'telephone',
+                        type: 'tel',
+                        error: output.orderResult.errors?.telephone
+                    })}
+                    ${textField({
+                        label: translation.email,
+                        name: 'email',
+                        error: output.orderResult.errors?.email
+                    })}
+                    ${textareaField({
+                        label: translation.comment,
+                        name: 'comment',
+                        error: output.orderResult.errors?.comment
+                    })}
+                    <button type="submit" ?disabled=${output.orderResult.pending}>
+                        ${translation.approveOrder} (${formatPrice(output.totalPrice)})
+                    </button>
+                </form>
+                <button type="button" @click=${handleCloseClick}>${translation.close}</button>
+            ` : cart.section === SectionType.COMPLETE ? html`
+                <div>
+                    ${translation.orderCompleted}
+                </div>
+                <button type="button" @click=${handleCloseClick}>${translation.close}</button>
+            ` : null}
+        </div>
     `, element);
 }
 
